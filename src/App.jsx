fde3f2867859441c69f8722347adc72847bf9e79
Agustin <agustinsebastianrios@gmail.com>
2024-03-28 11:47:48 -0300

    primer

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Footer from "./Footer"
+function App() {
+  return <div>
+     <h1>UltimaFicha</h1>
+
+<Footer />
+
+</div>
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = ()=>{
+    return <h1>Derechos reservados Ultimaficha 2024</h1>
+}
+
+export default Footer
